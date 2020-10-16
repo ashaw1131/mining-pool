45c994fd5391ec77899302026c865c7303c860cb
ashaw1131 <ashaw1131@gmail.com>
2020-10-16 16:13:57 -0500

    code base update

--- storage/redis.go
+++ storage/redis.go
@@ -1,11 +1,12 @@
 package storage
 
 import (
+	"context"
 	"strconv"
 	"strings"
 	"time"
 
-	"github.com/go-redis/redis/v7"
+	"github.com/go-redis/redis/v8"
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/mining-pool/not-only-mining-pool/config"
 	"github.com/mining-pool/not-only-mining-pool/types"
@@ -22,9 +23,10 @@ func NewStorage(coinName string, options *config.RedisOptions) *DB {
 	client := redis.NewClient(options.ToRedisOptions())
 	if client == nil {
 		log.Panic("failed to connect to the redis server. If you dont wanna db storage please delete redis config in config file")
+		return nil
 	}
 
-	result, err := client.Ping().Result()
+	result, err := client.Ping(context.Background()).Result()
 	if err != nil || strings.ToLower(result) != "pong" {
 		log.Panicf("failed to connect to the redis server: %s %s", result, err)
 	}
@@ -40,64 +42,167 @@ func (s *DB) PutShare(share *types.Share, accepted bool) {
 	strNow := strconv.FormatInt(now, 10)
 
 	ppl := s.Pipeline()
+	ctx := context.Background()
+
+	strDiff := strconv.FormatFloat(share.Diff, 'f', 5, 64)
+	ppl.SAdd(ctx, s.coin+":pool:miners", share.Miner)              // miner index
+	ppl.SAdd(ctx, s.coin+":miner:"+share.Miner+":rigs", share.Rig) // rig index
+
 	if share.ErrorCode == 0 {
-		ppl.HIncrByFloat(s.coin+":shares:roundCount", share.Miner, share.Diff)
-		ppl.HIncrBy(s.coin+":stats", "validShares", 1)
-		ppl.ZAdd(s.coin+":hashrate", &redis.Z{
-			Score: float64(now),
-			Member: strings.Join([]string{
-				strconv.FormatFloat(share.Diff, 'f', 5, 64),
-				share.Miner,
-				strNow,
-			}, ":"),
+		ppl.HIncrByFloat(ctx, s.coin+":miner:contrib", share.Miner, share.Diff)
+		ppl.HIncrBy(ctx, s.coin+":miner:shares:valid", share.Miner, 1)
+
+		ppl.HIncrBy(ctx, s.coin+":pool", "validShares", 1)
+
+		ppl.ZAdd(ctx, s.coin+":pool:shares", &redis.Z{
+			Score:  float64(now),
+			Member: strDiff,
 		})
+
+		ppl.ZAdd(ctx, s.coin+":miner:"+share.Miner+":hashes", &redis.Z{
+			Score:  float64(now),
+			Member: strDiff,
+		})
+
+		ppl.ZAdd(ctx, s.coin+":miner:"+share.Miner+":rig:"+share.Rig+":hashes", &redis.Z{
+			Score:  float64(now),
+			Member: strDiff,
+		})
+
 	} else {
-		ppl.HIncrBy(s.coin+":stats", "invalidShares", 1)
+		ppl.HIncrBy(ctx, s.coin+":miner:shares:invalid", share.Miner, 1)
+		ppl.IncrBy(ctx, s.coin+":pool:shares:invalid", 1)
 	}
 
 	// when mined one => seal roundCount,
 	// BlockHex is not accuracy, maybe out of date
 	if len(share.BlockHex) > 0 {
 		if accepted {
-			ppl.Rename(s.coin+":shares:roundCount", s.coin+":shares:round"+strconv.FormatInt(share.BlockHeight, 10))
-			//ppl.Rename(s.coin+":shares:timesCount", strings.Join([]string{
-			//	s.coin,
-			//	"shares:round",
-			//	strconv.FormatInt(share.BlockHeight, 10),
-			//}, ":"))
-			ppl.SAdd(s.coin+":blocksPending", strings.Join([]string{
-				s.coin,
-				share.BlockHash,
+			ppl.Rename(ctx, s.coin+":shares:round", s.coin+":shares:round:"+strconv.FormatInt(share.BlockHeight, 10))
+			ppl.SAdd(ctx, s.coin+":blocks:pending", share.BlockHash)
+			ppl.HSetNX(ctx, s.coin+":blocks", share.BlockHash, strings.Join([]string{
 				share.TxHash,
 				strconv.FormatInt(share.BlockHeight, 10),
 				share.Miner,
 				strNow,
 			}, ":"))
-			ppl.HIncrBy(s.coin+":stats", "validBlocks", 1)
+
+			ppl.HIncrBy(ctx, s.coin+":pool", "validBlocks", 1)
 		} else {
-			ppl.HIncrBy(s.coin+":stats", "invalidBlocks", 1)
+			ppl.HIncrBy(ctx, s.coin+":pool", "invalidBlocks", 1)
 		}
 	}
 
-	_, err := ppl.Exec()
+	_, err := ppl.Exec(ctx)
 	if err != nil {
 		log.Error(err)
 	}
 }
 
-func (s *DB) PutPendingBlockHash(blockHash string) {
-	s.Client.SAdd(s.coin+":stats:blockPending", blockHash)
+func (s *DB) GetMinerIndex() ([]string, error) {
+	return s.SMembers(context.Background(), s.coin+":pool:miners").Result()
 }
 
-// TODO
-func (s *DB) GetShares() []*types.Share {
-	// s.Client.Z
-	return nil
+func (s *DB) GetRigIndex(minerName string) ([]string, error) {
+	return s.SMembers(context.Background(), s.coin+":miner:"+minerName+":rigs").Result()
 }
 
-func (s *DB) GetStats() {
+// GetCurrentRoundCount will return a total diff of shares the miner submitted
+func (s *DB) GetMinerCurrentRoundContrib(minerName string) (float64, error) {
+	return s.HGet(context.Background(), s.coin+":shares:contrib", minerName).Float64()
 }
 
-//             ["scard", ":blocksPending"],
-//            ["scard", ":blocksConfirmed"],
-//            ["scard", ":blocksKicked"]
+// GetMinerTotalShares will return the number of all valid shares
+func (s *DB) GetPoolTotalValidShares() (uint64, error) {
+	return s.HGet(context.Background(), s.coin+":pool", "validShares").Uint64()
+}
+
+// GetMinerTotalShares will return the number of all valid blocks
+func (s *DB) GetPoolTotalValidBlocks() (uint64, error) {
+	return s.HGet(context.Background(), s.coin+":pool", "validBlocks").Uint64()
+}
+
+// GetMinerTotalShares will return the number of all invalid shares
+func (s *DB) GetPoolTotalInvalidShares() (uint64, error) {
+	return s.HGet(context.Background(), s.coin+":pool", "validShares").Uint64()
+}
+
+// GetMinerTotalShares will return the number of all invalid blocks
+func (s *DB) GetPoolTotalInvalidBlocks() (uint64, error) {
+	return s.HGet(context.Background(), s.coin+":pool", "invalidBlocks").Uint64()
+}
+
+// GetMinerTotalShares will return the number of all invalid blocks
+func (s *DB) GetRigHashrate(minerName, rigName string, from, to int64) (hashrate float64, err error) {
+	slice, err := s.ZRange(context.Background(), s.coin+":miner:"+minerName+":rig:"+rigName+":hashes", from, to).Result()
+	if err != nil {
+		return 0.0, err
+	}
+
+	var totalDiff float64
+	for i := range slice {
+		diff, err := strconv.ParseFloat(slice[i], 64)
+		if err != nil {
+			return 0.0, err
+		}
+
+		totalDiff += diff
+	}
+
+	return totalDiff / float64(to-from), nil
+}
+
+// GetMinerTotalShares will return the number of all invalid blocks
+func (s *DB) GetMinerHashrate(minerName string, from, to int64) (hashrate float64, err error) {
+	slice, err := s.ZRange(context.Background(), s.coin+":miner:"+minerName+":shares", from, to).Result()
+	if err != nil {
+		return 0.0, err
+	}
+
+	var totalDiff float64
+	for i := range slice {
+		diff, err := strconv.ParseFloat(slice[i], 64)
+		if err != nil {
+			return 0.0, err
+		}
+
+		totalDiff += diff
+	}
+
+	return totalDiff / float64(to-from), nil
+}
+
+// GetMinerTotalShares will return the number of all invalid blocks
+func (s *DB) GetPoolHashrate(from, to int64) (float64, error) {
+	slice, err := s.ZRange(context.Background(), s.coin+":pool:shares", from, to).Result()
+	if err != nil {
+		return 0.0, err
+	}
+
+	var totalDiff float64
+	for i := range slice {
+		diff, err := strconv.ParseFloat(slice[i], 64)
+		if err != nil {
+			return 0.0, err
+		}
+
+		totalDiff += diff
+	}
+
+	return totalDiff / float64(to-from), nil
+}
+
+// GetCurrentRoundCount will return a total diff of shares the miner submitted
+func (s *DB) GetMinerRigs(minerName string) (float64, error) {
+	return s.HGet(context.Background(), s.coin+":shares:contrib", minerName).Float64()
+}
+
+// ConfirmBlock alt one pending block to confirmed
+func (s *DB) ConfirmBlock(blockHash string) (ok bool, err error) {
+	return s.SMove(context.Background(), s.coin+":blocks:pending", s.coin+":blocks:confirmed", blockHash).Result()
+}
+
+// KickBlock alt one pending block to kicked
+func (s *DB) KickBlock(blockHash string) (ok bool, err error) {
+	return s.SMove(context.Background(), s.coin+":blocks:pending", s.coin+":blocks:kicked", blockHash).Result()
+}
